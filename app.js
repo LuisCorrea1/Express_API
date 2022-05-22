83b8daebfaf3834f19074b191bf0b9a0b2f7c232
luisSCV072 <57187479+luisSCV072@users.noreply.github.com>
2022-05-21 20:15:34 -0500

    Add GET method (endpoint)

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ const port = 3000
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
+
+//ROUTES
+//HTTP Methods - GET, POST, PUT, DELETE
+app.get('/v1/explorers', (req,res) => {
+    console.log(`API explorers GET ALL requests ${new Date()}`)
+    const explorer1 = {id:1, name:"Luis1"}
+    const explorer2 = {id:2, name:"Luis2"}
+    const explorer3 = {id:3, name:"Luis3"}
+    const explorer4 = {id:4, name:"Luis4"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    //HTTP CODE STATUS
+    res.status(200).json(explorers)
+})
